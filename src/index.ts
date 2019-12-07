de337826f384e0af3d847c04e08f39629e790f8c
jiftle <lixugood@126.com>
2019-12-07 17:25:59 +0000

    w

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import {
   EverSyncClient
 } from "./eversyncclient";
 
+import Converter from "./converterplus";
+
 
 
 function greeter(person) {
@@ -11,6 +13,11 @@ function greeter(person) {
 let user = "Jane User";
 
 console.log(greeter(user));
+
+//let markdown = "# 你好啊，我的Markdown笔记\n\n## 二级标题\n- 来自EverSynClient";
+let markdown = "你好啊我的Markdown笔记";
+
+const converter = new Converter({});
 
 
 //参数配置
@@ -30,31 +37,38 @@ client.syncAccount(config.token, config.noteStoreUrl).then(function(){
 
     // 列出笔记列表
     //    var notes = client.listNotes(notebooks[0]);
-    //    console.log(notes[0]);
-    //
-    //    // 获取笔记内容
-    //    var note = notes[0]
-    //var noteContent = client.getNoteContent(note.guid)
-    //console.log(noteContent);
+    //   console.log(notes[0]);
+    //    for(var i in notebooks) {
+    //        console.log(notebooks[i])
+    //        
+    //        // 获取笔记内容，异步方式
+    //        var note = notebooks[i]
+    //        client.getNoteContent(note).then(function(noteContent){
+    //            console.log(noteContent);
+    //        });
+    //    }
 
-    // 异步方式
-    // var noteContent = client.getNoteContent(note.guid).then(function(noteContent){
-    //     console.log(noteContent);
-    // });
-    
+
+
+    converter.toEnml(markdown).then(function(enml){
+        console.log(enml);
+
+        // 异步方式
         // ------------------ 新建笔记 ----------------
         let meta = {
             title:"EverSyncClient Test",
             tags: "markdown",
             notebook: "blog"
         }
-    let content = ""
-    //        let content = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n' +
-    //    '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">\n' +
-    //    '<en-note><div><en-media hash="a8d37852fba2d22e11c93e4459d4263d" title="Attachment" width="3024" type="image/jpeg"/></div><div><en-media hash="cc6089c8269ed3ab53fb0cd59596f11e" title="Attachment" width="2070" type="image/png"/></div><div><en-media hash="57dd10ddca3fbcbfdf4ab96a08e69616" title="Attachment" width="2752" type="image/png"/></div><div><br/></div></en-note>\n'
-        let resources = 0;
-        var a = client.createNote(meta,content,resources);
-        console.log(a);
+        let content = enml;
+        //            let content = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n' +
+        //        '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">\n' +
+        //        '<en-note><div><en-media hash="a8d37852fba2d22e11c93e4459d4263d" title="Attachment" width="3024" type="image/jpeg"/></div><div><en-media hash="cc6089c8269ed3ab53fb0cd59596f11e" title="Attachment" width="2070" type="image/png"/></div><div><en-media hash="57dd10ddca3fbcbfdf4ab96a08e69616" title="Attachment" width="2752" type="image/png"/></div><div><br/></div></en-note>\n'
+                let resources = 0;
+        client.createNote(meta,content,resources).then(function(nte) {
+             console.log("note upload success!\n-------------------\nnoteGuid=" + nte.guid + "\nnoteTitle=" + nte.title + "\nnotebookGuid=" + nte.notebookGuid + "\n");
+        });
+    });
 
     // ------------- 更新笔记 -------------------
     //   let noteGuid = "" ;
